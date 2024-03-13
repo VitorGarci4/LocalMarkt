4d1bb529c38ea3f9b55668512c4c697e80eb0c54
unknown <VitorGarci4>
2024-03-13 16:51:45 -0300

    dados da loja

---
+++ store/script.js
@@ -0,0 +1,12 @@
+let produtos;
+
+window.onload = function(){
+    var storedUser = localStorage.getItem("user")
+    var user = JSON.parse(storedUser);
+    
+    document.getElementById("user").textContent
+    document.getElementById("perfil").textContent
+    document.getElementById("idPerfil").textContent
+
+
+}
